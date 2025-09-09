3b33289f22061553b5a75d769627ce77479f3793
dong7812 <dlehdrb020@gmail.com>
2025-09-09 21:17:53 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 852 ms, Memory: 143220 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,9 +1,49 @@
 import sys
 
-rd = sys.stdin.buffer.readline
+sys.setrecursionlimit(1_000_000) 
 
-n = int(rd())
+rd = sys.stdin.read().splitlines
 
-arr = [int(rd()) for _ in range(n)]
-arr.sort()
-sys.stdout.write('\n'.join(map(str, arr)))
+input_value = rd()
+todo_arr = int(input_value.pop(0))
+input_value = list(map(int, input_value))
+
+def sort(arr):
+    if int(len(arr)) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    right = []
+    left = []
+    mid = []
+    
+    for i in range(0, len(arr)):
+        if arr[i] < pivot:
+            left.append(arr[i])
+        elif arr[i] > pivot:
+            right.append(arr[i])
+        else:
+            mid.append(arr[i])
+    
+    if len(left) != 1:
+        left = sort(left)
+    
+    if len(right) != 1:
+        right = sort(right)
+        
+    return left + mid + right
+        
+    
+def main():
+    if len(input_value) > 100000:                
+        input_value.sort()
+        sys.stdout.write("\n".join(map(str, input_value)))
+    else:                                         
+        res = sort(input_value)
+        sys.stdout.write("\n".join(map(str, res)))
+    
+if __name__ == "__main__" :
+    main()
+    
+    
+
